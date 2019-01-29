8b8bb58763ba27722400b4bbcacf378b29f12858
Soufian <soufian.el.hossary@gmail.com>
2019-01-29 14:41:36 +0200

    Initial Commit

---
+++ TemplateCreator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TemplateCreator
+//
+//  Created by Soufian Hossam on 1/29/19.
+//  Copyright © 2019 SICT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TemplateCreator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TemplateCreator
+//
+//  Created by Soufian Hossam on 1/29/19.
+//  Copyright © 2019 SICT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
