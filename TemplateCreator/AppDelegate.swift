8e9da6593201dbc630bd84dc3985e238445ed880
Soufian <soufian.el.hossary@gmail.com>
2019-02-03 16:44:53 +0200

    before converting to top menu app.

--- TemplateCreator/AppDelegate.swift
+++ TemplateCreator/AppDelegate.swift
@@ -11,16 +11,29 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = #imageLiteral(resourceName: "generator")
+        }
+        
+        constructMenu()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(printQuote), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
     }
-
-
+    
+    @objc func printQuote() {
+        print(123)
+    }
 }
 
